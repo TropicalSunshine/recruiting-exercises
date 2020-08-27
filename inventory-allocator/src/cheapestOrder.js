84eed7e97d68615b619ab8fd1b190fb2d16db7fc
Jason C. Liu <32473162+TropicalSunshine@users.noreply.github.com>
2020-08-27 11:05:33 -0700

    organize structure, install jest, jsdoc

---
+++ inventory-allocator/src/cheapestOrder.js
@@ -0,0 +1,12 @@
+/**
+ * Finds the cheapest way an object can be shipped given an object of orders and an array of warehouses
+ * @param {Object} orders 
+ * @param {Array} warehouses 
+ * @returns {Array}
+ */
+const cheapestOrder = (
+    orders = {}, //orders must be an arra
+    warehouses = []
+) => {
+    
+}
